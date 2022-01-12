f893a9cab8d4c1e6d99ee2b08fc265346d51bf7b
vincenthuang113 <vincenthuang113@gmail.com>
2022-01-12 18:01:14 -0500

    day_3 hw

---
+++ day_3/module.py
@@ -0,0 +1,11 @@
+def printName(name):
+    print(f"Hello Mr/Ms {name}...we've been waiting for you!")
+
+def sqft(L,W):
+    area = L * W
+    print(f"The house is {area} sqft.")
+
+import math 
+def circum(radius):
+    c = radius*math.pi*2
+    print(f"The circumference is {c}.")
